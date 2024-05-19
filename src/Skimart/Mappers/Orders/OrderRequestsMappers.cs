b400a247b47eabd90ebb7294011bbf4b41d57a2f
fajrocha_apc <fabio.jr@ua.pt>
2024-05-19 14:27:06 +0100

    Further cleanup.

---
+++ src/Skimart/Mappers/Orders/OrderRequestsMappers.cs
@@ -0,0 +1,28 @@
+﻿using Skimart.Application.Orders.Commands.CreateOrder;
+using Skimart.Contracts.Orders.Requests;
+
+namespace Skimart.Mappers.Orders;
+
+public static class OrderRequestsMappers
+{
+    public static CreateOrderCommand ToCommand(this OrderRequest orderRequest)
+    {
+        return new CreateOrderCommand(
+            orderRequest.BasketId,
+            orderRequest.DeliveryMethodId,
+            orderRequest.ShippingAddress.ToCommand());
+    }
+    
+    private static CreateOrderShippingAddressCommand ToCommand(this ShippingAddressRequest shippingAddressRequest)
+    {
+        return new CreateOrderShippingAddressCommand(
+            shippingAddressRequest.FirstName,
+            shippingAddressRequest.LastName,
+            shippingAddressRequest.Street,
+            shippingAddressRequest.City,
+            shippingAddressRequest.Province,
+            shippingAddressRequest.ZipCode);
+    }
+
+    
+}
